eecb57b752b1899ad406395c37b49e6b94b16f75
Ananya Subramaniam <149892286+ananyasub@users.noreply.github.com>
2026-03-09 23:18:39 -0400

    I/O and CPU processes are started

--- PART1.c
+++ PART1.c
@@ -5,15 +5,16 @@
 #include <string.h>
 #include <math.h>
 
-double next_exp(float lambda, float upper){
+double next_exp(double lambda, double upper) {
+    double r, val;
 
-    while (val > upper){
-        double r = drand(48); 
-        double val = -log(r)/lambda;
-        return val;
-    }
+    do {
+        r = drand48();
+        val = -log(r) / lambda;
+    } while (val > upper);
+
+    return val;
 }
-
 int main( int argc, char ** argv ){
 
     if (argc != 6){
@@ -56,6 +57,25 @@ int main( int argc, char ** argv ){
         printf("<<< -- process set (n=%d) with %d CPU-bound processes\n", n, ncpu );
     }
     printf("<<< -- seed=%d; lambda=%06f; upper bound=%.0f\n", seed, lambda, upper);
+
+    // char wletter = 'A' + (11/10);
+    // printf("letter: %c\n", wletter);
+    for (int i = 0; i < n; i++) {
+        char letter = 'A' + (i / 10);
+        int digit = i % 10;
+        int is_cpu_bound = (i >= (n - ncpu));
+
+        int arrival = (int) floor(next_exp(lambda, upper));
+        int bursts = (int) ceil(16 * drand48());
+
+        if (is_cpu_bound) {
+            printf("CPU-bound process %c%d: arrival time %dms; %d CPU bursts:\n",
+                letter, digit, arrival, bursts);
+        } else {
+            printf("I/O-bound process %c%d: arrival time %dms; %d CPU bursts:\n",
+                letter, digit, arrival, bursts);
+        }
+    }
 
 
     return EXIT_SUCCESS;
